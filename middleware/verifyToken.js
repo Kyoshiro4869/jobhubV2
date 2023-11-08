b3d01731a2b010628c70c6361193d5c9d8584679
Kyoshiro4869 <kyoshiro441@gmail.com>
2023-11-08 19:34:19 +0900

    debg

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,37 +1,40 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res ,next) =>{
+const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if(authHeader){
-    const token = authHeader.spilt("")[1];
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
 
-    jwt.verify(token,process.env.JWT_SEC,async(err,user) => {
-      if(err){
-        return res.status(401).json({message:"Invalid token"})
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid token" });
       }
-      res.user = user;
+      req.user = user;
       next();
-    })
-  } 
+    });
+  } else {
+    return res.status(401).json({ message: "Authorization header not found" });
+  }
 };
 
-const verifyAndAuth = (req, res , next) => {
-  verifyToken(req,res,() => {
-    if(req.user.id || req.user.isAdimin){
+const verifyAndAuth = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id || req.user.isAdmin) {
       next();
-    }else{
-      return res.status(403).json({message:"You are not authorized to access"})
+    } else {
+      return res.status(403).json({ message: "You are not authorized to access" });
     }
-  })
-}
-const verifyAgent = (req, res , next) => {
-  verifyToken(req,res,() => {
-    if(req.user.isAgent || req.user.isAdimin){
-      next();
-    }else{
-      return res.status(403).json({message:"You are not authorized to access"})
-    }
-  })
-}
+  });
+};
 
-module.exports = {verifyToken, verifyAndAuth,verifyAgent}
+const verifyAgent = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAgent || req.user.isAdmin) {
+      next();
+    } else {
+      return res.status(403).json({ message: "You are not authorized to access" });
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyAndAuth, verifyAgent };
