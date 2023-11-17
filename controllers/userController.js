48085607a88fadcdc2c349b98a3203009dfb9f28
Kyoshiro4869 <kyoshiro441@gmail.com>
2023-11-18 01:52:19 +0900

    f

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,11 +19,14 @@ module.exports = {
   },
   getUser: async(req,res)=>{
     try{
+      console.log('getUser function called'); // Debug log
       const profile = await User.findById(req.user.id)
       const {password,createdAt,updatedAt,__v,...userData} = profile.doc;
-
+  
+      console.log('User data retrieved: ', userData); // Debug log
       res.status(200).json(userData)
     } catch (error){
+      console.log('Error in getUser: ', error.message); // Debug log
       res.status(500).json({error:error})
     }
   }
